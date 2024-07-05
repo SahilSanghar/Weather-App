e1a4b6ffd26bab577f576c32cd7c06cd5e9847c3
SAHIL sanghar <sangharsahil546@gmail.com>
2024-07-05 07:35:31 +0530

    Update: Removed unnecessary part

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,8 +3,6 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import "./SearchBox.css";
-import { tr } from '@faker-js/faker';
-// import { response } from 'express';
 
 const SearchBox = ({updateInfo}) => {
     let [city, setCity] = useState("");
